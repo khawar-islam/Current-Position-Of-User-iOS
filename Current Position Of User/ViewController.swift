d4efea62f202542b27a84b8a91d79459d411465e
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2017-10-27 21:05:46 +0500

    Initial Commit

---
+++ Current Position Of User/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Current Position Of User
+//
+//  Created by MacBook Pro on 27/10/2017.
+//  Copyright © 2017 MacBook Pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
